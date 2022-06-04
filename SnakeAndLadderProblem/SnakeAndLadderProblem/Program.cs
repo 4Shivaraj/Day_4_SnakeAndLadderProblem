5811959f8ee93c174ad275e243acb7f22806a249
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-05 01:29:35 +0530

    [Shivaraj] Added Single player at start position problem solution

--- SnakeAndLadderProblem/SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/SnakeAndLadderProblem/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Welcome To Snake And Ladder Problem");
+﻿//Console.WriteLine("Welcome To Snake And Ladder Problem");
+using System;
+namespace SnakeAndLadderProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Snake And Ladder Game========");
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.StartGame(); // Call the Method.
+            Console.ReadLine();
+        }
+    }
+}
